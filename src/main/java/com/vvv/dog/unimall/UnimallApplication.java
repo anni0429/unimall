cb8fa599c5b93716348f515168ece656d95f0a00
zhangtsh <taosheng.zhang@qzingtech.com>
2023-04-21 11:38:14 +0800

    init

---
+++ src/main/java/com/vvv/dog/unimall/UnimallApplication.java
@@ -0,0 +1,13 @@
+package com.vvv.dog.unimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimallApplication.class, args);
+	}
+
+}
